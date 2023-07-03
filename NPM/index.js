bac805d19238bef36816b3173b9069cc964b375c
Bharath Ravelli <bharathravelli419@gmail.com>
2023-07-03 22:30:59 +0530

    QR-Code generator to my website

--- NPM/index.js
+++ NPM/index.js
@@ -1,12 +1,38 @@
-// var generateName = require("sillyname");
-// var sillyName = generateName();
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
 
-// console.log(`My name is ${sillyName}`);
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        message:"Type in Your URL",
+        name :"URL"
+    }
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url = answers.URL;
+      //QR Code generation and Saving Part 
+  var qr_svg = qr.image(url, { type: 'png' });
+  qr_svg.pipe(fs.createWriteStream('A_QR.png'));
+   
+  fs.writeFile("URL.txt",url,(err)=>{
+    if(err)
+    throw err
+    else
+    console.log("The file has been craeted")
 
-import generateName from "sillyname"
-var sillyName = generateName();
-console.log(sillyName)
+  })
+  
+  })
+  .catch((error) => {
+    if (error) {
+     console.log(err);
+    } else {
+      // Something else went wrong
+      console.log("Everything is Clear")
+    }
+  });
 
-import superheroes from "superheroes"
 
-console.log(`Super Hero name is :${superheroes.random()}`)
