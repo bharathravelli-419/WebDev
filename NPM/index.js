59524acdd6bc9facfe2fbd683b26a6f5d32d9d24
Bharath Ravelli <bharathravelli419@gmail.com>
2023-07-03 21:48:06 +0530

    NPM intro and Node.js

---
+++ NPM/index.js
@@ -0,0 +1,12 @@
+// var generateName = require("sillyname");
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}`);
+
+import generateName from "sillyname"
+var sillyName = generateName();
+console.log(sillyName)
+
+import superheroes from "superheroes"
+
+console.log(`Super Hero name is :${superheroes.random()}`)
